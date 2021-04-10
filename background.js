385ec3cdd4ad06d18cfdff8318cb40cf8fadb70b
Max Gomes <maxgomes92@live.com>
2021-04-10 18:52:26 +0200

    Handle form data

--- background.js
+++ background.js
@@ -1,3 +1,23 @@
+// active: true
+// audible: true
+// autoDiscardable: true
+// discarded: false
+// favIconUrl: "https://web.whatsapp.com/img/favicon_c5088e888c97ad440a61d247596f88e5.png"
+// groupId: -1
+// height: 899
+// highlighted: true
+// id: 17
+// incognito: false
+// index: 0
+// mutedInfo: {muted: false}
+// pinned: true
+// selected: true
+// status: "loading"
+// title: "WhatsApp"
+// url: "https://web.whatsapp.com/"
+// width: 1075
+// windowId: 16
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   // if (tab.url === 'https://web.whatsapp.com/' && tab.status === 'complete') {
     chrome.pageAction.show(tabId);
--- templates.js
+++ templates.js
@@ -1,7 +1,62 @@
-document.getElementById('btn-contact-add').addEventListener('click', (evt) => {
+const l = console.log
+
+const template = {
+  id: getRandomId(),
+  title: 'Template zica',
+  message: 'Fala $nome, você tem $idade anos.'
+}
+
+function main () {
+
+  setupForm()
+
+  // Temp
+  addNewContact()
+  addNewContact()
+  updateContactVariables()
+
+  document.getElementById('btn-contact-add').addEventListener('click', addNewContact)
+  document.getElementById('btn-submit').addEventListener('click', onSubmit)
+  document.getElementById('btn-delete').addEventListener('click', onDelete)
+}
+
+function onDelete () {
+  console.log('delete')
+}
+
+function onSubmit () {
+  const formData = getFormData()
+
+  console.log('onSubmit', formData)
+}
+
+function getFormData () {
+  const contactsList = document.getElementById('contacts-list').childNodes
+
+  const contacts = Array.from(contactsList).map((contactEl) => {
+    const contactName = contactEl.querySelector('input[id^="contact-"]').value
+    const contactData = getCurrentVariablesFromMessage().reduce((acc, cur) => {
+      acc[cur] = contactEl.querySelector(`input[id^="${cur}_"]`).value
+      return acc
+    }, {})
+
+    return {
+      contactName,
+      contactData,
+    }
+  }, {})
+
+
+  return {
+    title: document.getElementById('title').value,
+    message: document.getElementById('message').value,
+    contacts,
+  }
+}
+
+function addNewContact () {
   const contactsList = document.getElementById('contacts-list')
-  const contactCardTemplateFragment = document.getElementById('contact-card-template')
-  const contactCardTemplate = document.importNode(contactCardTemplateFragment.content, true);
+  const contactCardTemplate = document.importNode(document.getElementById('contact-card-template').content, true);
   const contactCard = contactCardTemplate.getElementById('contact-card')
 
   const removeButton = contactCard.querySelector('button')
@@ -10,4 +65,74 @@ document.getElementById('btn-contact-add').addEventListener('click', (evt) => {
   })
 
   contactsList.appendChild(contactCard)
-})
+}
+
+function getRandomId () {
+  return Math.random().toString(32)
+}
+
+function getFormVariablesEl (variableName) {
+  const formVariablesTemplate = document.importNode(document.getElementById('form-variables-template').content, true);
+  const formVariablesEl = formVariablesTemplate.getElementById('form-variables')
+
+  const inputId = `${variableName}_${getRandomId()}`
+
+  formVariablesEl
+    .querySelector('input')
+    .setAttribute('id', inputId)
+
+  formVariablesEl
+    .querySelector('label')
+    .innerHTML = variableName
+
+  formVariablesEl
+    .querySelector('label')
+    .setAttribute('for', inputId)
+
+
+  return formVariablesEl
+}
+
+function updateContactVariables () {
+  const contactsListNodes = Array.from(document.getElementById('contacts-list').childNodes)
+  const variables = getCurrentVariablesFromMessage()
+
+  for (let node of contactsListNodes) {
+    for (let variable of variables) {
+      node
+        .querySelector('#form-variables-list')
+        .appendChild(getFormVariablesEl(variable))
+    }
+  }
+}
+
+function getCurrentVariablesFromMessage () {
+  // const formData = new FormData(document.querySelector('form'))
+  const message = document.getElementById('message').value
+  const regex = new RegExp(/\$\w+/g)
+  const variables = []
+
+  let match
+  do {
+    match = regex.exec(message)
+
+    if (match && match[0]) {
+      variables.push(match[0])
+    }
+
+  } while (match)
+
+  return variables
+}
+
+function setupForm() {
+  Object.entries(template).forEach(([key, value]) => {
+    const el = document.getElementById(key)
+
+    if (el) {
+      el.value = value
+    }
+  });
+}
+
+main()
