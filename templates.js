884a6b5b176778c7d701f44a83a1c88fae34d346
Max Gomes <maxgomes92@live.com>
2021-04-10 14:10:52 +0200

    Add and remove template on click

--- templates.js
+++ templates.js
@@ -1,4 +1,13 @@
-
 document.getElementById('btn-contact-add').addEventListener('click', (evt) => {
+  const contactsList = document.getElementById('contacts-list')
+  const contactCardTemplateFragment = document.getElementById('contact-card-template')
+  const contactCardTemplate = document.importNode(contactCardTemplateFragment.content, true);
+  const contactCard = contactCardTemplate.getElementById('contact-card')
 
+  const removeButton = contactCard.querySelector('button')
+  removeButton.addEventListener('click', () => {
+    contactsList.removeChild(contactCard)
+  })
+
+  contactsList.appendChild(contactCard)
 })
